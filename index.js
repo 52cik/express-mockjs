e81c44d8a9212050e2dddeb2b8966fa19c99b66b
楼教主 <cik520@qq.com>
2016-09-29 10:36:31 +0800

    add api list

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ var Mock = require('mockjs');
 var walkdir = require('node-walkdir');
 
 var RE_URL = /^\s*\/(?:\/\s*@url\s+(\/[^ \n]+)|\*[\s\S]+?@url\s+(\/[^\n]+)[\s\S]+?\*\/)/im;
+var RE_DOC = /^\s*\/\*[*\s]+?([^\r\n]+)[\s\S]+?@url\s+(\/[^\n]+)/;
 
 module.exports = function (dir) {
   var route = {}; // route list
+  var docs = {}; // document list
 
   walkdir(dir, '.json', function (filename) {
     var url = '/' + path.basename(filename, '.json');
     var content = fs.readFileSync(filename, 'utf8');
 
-    var m = content.match(RE_URL);
-    if (m) {
-      url = m[1] || m[2] || url;
-    }
+    url = getUrl(content, url);
 
-    if (route[url]) {
+    if (route[url] || docs[url]) {
       console.warn('[Mock]: URL already exists and has been covered with new data.');
     }
+
+    docs[url] = getDoc(content, filename);
 
     try {
       var json = new Function('return (' + content + ')')();
@@ -31,13 +32,44 @@ module.exports = function (dir) {
     }
   });
 
+  function getUrl(content, url) {
+    var m = content.match(RE_URL);
+
+    if (m) {
+      url = (m[1] || m[2] || url).trim();
+    }
+
+    return url;
+  }
+
+  function getDoc(content, filename) {
+    var m = content.match(RE_DOC);
+
+    if (m) {
+      return m[1].replace(/(^[\s*]+|[\s*]+$)/g, '');
+    }
+
+    return '[file:' + filename + '] no description!';
+  }
+
   return function (req, res, next) {
     var url = req.url.split('?')[0];
 
     if (route[url]) {
       res.json(Mock.mock(route[url]));
     } else {
-      next();
+      // next();
+      var host = req.protocol + '://' + req.headers.host + req.baseUrl;
+      var html = JSON.stringify(docs, function (k, v) {
+        if (k[0] === '/') {
+          return '<a href=' + (host + k) + '>' + v + '</a>';
+        }
+        return v;
+      }, '  ');
+      
+      res.type('html');
+      // Add b tags to prevent JSON Viewer parsing
+      res.end('<b><pre>' + html);
     }
   };
 
