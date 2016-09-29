0a68a446b2a536767783c0f288cf527659fed604
楼教主 <cik520@qq.com>
2016-09-29 13:17:51 +0800

    fix bug

--- index.js
+++ index.js
@@ -3,54 +3,45 @@ var path = require('path');
 var Mock = require('mockjs');
 var walkdir = require('node-walkdir');
 
-var RE_URL = /^\s*\/(?:\/\s*@url\s+(\/[^ \n]+)|\*[\s\S]+?@url\s+(\/[^\n]+)[\s\S]+?\*\/)/im;
-var RE_DOC = /^\s*\/\*[*\s]+?([^\r\n]+)[\s\S]+?@url\s+(\/[^\n]+)/;
+var RE = /^\s*\/\*[*\s]+?([^\r\n]+)[\s\S]+?@url\s+(\/[^\n]+)[\s\S]+?\*\//im;
 
-module.exports = function (dir) {
+function mock(dir) {
   var route = {}; // route list
   var docs = {}; // document list
 
   walkdir(dir, '.json', function (filename) {
-    var url = '/' + path.basename(filename, '.json');
     var content = fs.readFileSync(filename, 'utf8');
 
-    url = getUrl(content, url);
+    var url = '/' + path.basename(filename, '.json');
+    var doc = '[file:' + filename + '] no description!';
 
-    if (route[url] || docs[url]) {
-      console.warn('[Mock]: URL already exists and has been covered with new data.');
+    var m = content.match(RE);
+
+    if (m) {
+      url = m[2].trim();
+      doc = m[1].replace(/(^[\s*]+|[\s*]+$)/g, '');
     }
 
-    docs[url] = getDoc(content, filename);
+    if (route[url] || docs[url]) {
+      mock.debug && console.warn('[Mock Warn]: "' + filename + ': ' + url + '" already exists and has been covered with new data.');
+    }
+
+    docs[url] = doc;
 
     try {
       var json = new Function('return (' + content + ')')();
+
       route[url] = json;
     } catch (e) {
+      var err = '"' + filename + ': ' + url + '" - Parse error!';
+
       route[url] = {
-        err: url + ' - Parse error!'
+        err: err
       }
+
+      mock.debug && console.warn('[Mock Warn]: ' + err);
     }
   });
-
-  function getUrl(content, url) {
-    var m = content.match(RE_URL);
-
-    if (m) {
-      url = (m[1] || m[2] || url).trim();
-    }
-
-    return url;
-  }
-
-  function getDoc(content, filename) {
-    var m = content.match(RE_DOC);
-
-    if (m) {
-      return m[1].replace(/(^[\s*]+|[\s*]+$)/g, '');
-    }
-
-    return '[file:' + filename + '] no description!';
-  }
 
   return function (req, res, next) {
     var url = req.url.split('?')[0];
@@ -58,7 +49,6 @@ module.exports = function (dir) {
     if (route[url]) {
       res.json(Mock.mock(route[url]));
     } else {
-      // next();
       var host = req.protocol + '://' + req.headers.host + req.baseUrl;
       var html = JSON.stringify(docs, function (k, v) {
         if (k[0] === '/') {
@@ -66,11 +56,15 @@ module.exports = function (dir) {
         }
         return v;
       }, '  ');
-      
+
       res.type('html');
-      // Add b tags to prevent JSON Viewer parsing
+
+      // Add b tags to prevent 'JSON Viewer' parsing.
       res.end('<b><pre>' + html);
     }
   };
 
-};
+}
+
+mock.debug = true;
+module.exports = mock;
